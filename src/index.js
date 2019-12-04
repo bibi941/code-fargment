702aa69153b22c6d3523dc3fac1f392417003b9b
成都-方欣芮 <sssrrr@vip.qq.com>
2019-12-04 17:34:59 +0800

    :sparkles:  改为面向对象解决 cache 缓存问题

--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
-let cache = []
-
-function findCache(source) {
-  for (let i = 0; i < cache.length; i++) {
-    if (cache[i][0] === source) {
-      return cache[i][1]
-    }
+class DeepCloner {
+  constructor() {
+    this.cache = []
   }
-  return undefined
-}
 
-function deepClone(source) {
-  // 复制对象
-  if (source instanceof Object) {
-    let cacheResult = findCache(source)
-    // 环检测-有缓存
-    if (cacheResult) {
-      return cacheResult
-    } else { // 无缓存
-      let result
-      if (source instanceof Array) {// 数组
-        result = []
-      } else if (source instanceof Function) {// 函数
-        result = function () {
-          return source.call(this,...arguments)
+  findCache(source) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === source) {
+        return this.cache[i][1]
+      }
+    }
+    return undefined
+  }
+
+  clone(source) {
+    // 复制对象
+    if (source instanceof Object) {
+      let cacheResult = this.findCache(source)
+      // 环检测-有缓存
+      if (cacheResult) {
+        return cacheResult
+      } else { // 无缓存
+        let result
+        if (source instanceof Array) {// 数组
+          result = []
+        } else if (source instanceof Function) {// 函数
+          result = function () {
+            return source.call(this,...arguments)
+          }
+        } else if (source instanceof RegExp) {// 正则表达式
+          result = new RegExp(source.source,source.flags)
+        } else if (source instanceof Date) {//日期
+          result = new Date(source)
+        } else { // 普通对象
+          result = {}
         }
-      } else if (source instanceof RegExp) {// 正则表达式
-        result = new RegExp(source.source,source.flags)
-      } else if (source instanceof Date) {//日期
-        result = new Date(source)
-      } else { // 普通对象
-        result = {}
+        this.cache.push([source,result])
+        for (let key in source) {
+          if (source.hasOwnProperty(key)) result[key] = this.clone(source[key]) //不复制原型树属性
+        }
+        return result
       }
-      cache.push([source,result])
-      for (let key in source) {
-        if (source.hasOwnProperty(key)) result[key] = deepClone(source[key]) //不复制原型树属性
-      }
-      return result
     }
+    // 复制基本数据结构
+    return source
   }
-  // 复制基本数据结构
-  return source
 }
 
-module.exports = deepClone
+module.exports = DeepCloner
--- test/index.js
+++ test/index.js
@@ -5,40 +5,40 @@ const deepClone = require('../src/index')
 chai.use(sinonChai)
 const assert = chai.assert
 
-describe('deepClone',() => {
-  it('是一个函数 ',() => {
-    assert.isFunction(deepClone)
+describe('new deepClone().clone',() => {
+  it('是一个类 ',() => {
+    assert.isFunction( deepClone)
   })
   it('可以复制基本类型',() => {
     // number
     const n1 = 1
-    const n2 = deepClone(n1)
+    const n2 = new deepClone().clone(n1)
     assert(n1 === n2)
     // string
     const s1 = 'bibi'
-    const s2 = deepClone(s1)
+    const s2 = new deepClone().clone(s1)
     assert(s1 === s2)
     // bool
     const b1 = false
-    const b2 = deepClone(b1)
+    const b2 = new deepClone().clone(b1)
     assert(b1 === b2)
     // null
     const empty = null
-    const empty2 = deepClone(empty)
+    const empty2 = new deepClone().clone(empty)
     assert(empty === empty2)
     // undefined
     const u1 = undefined
-    const u2 = deepClone(u1)
+    const u2 = new deepClone().clone(u1)
     assert(u1 === u2)
     // symbol
     const sy1 = Symbol()
-    const sy2 = deepClone(sy1)
+    const sy2 = new deepClone().clone(sy1)
     assert(sy1 === sy2)
   })
   describe('对象',() => {
     it('能够复制普通对象',() => {
       const a1 = { name: 'bibi',age: 18,child: { name: 's-bibi' } }
-      const a2 = deepClone(a1)
+      const a2 = new deepClone().clone(a1)
       assert(a1 !== a2)
       assert(a1.name === a2.name)
       assert(a1.age === a2.age)
@@ -47,7 +47,7 @@ describe('deepClone',() => {
     })
     it('能够复制数组 ',() => {
       const a1 = [[11,12],[22,23]]
-      const a2 = deepClone(a1)
+      const a2 = new deepClone().clone(a1)
       assert(a1 !== a2)
       assert(a1[0] !== a2[0])
       assert(a1[1] !== a2[1])
@@ -58,7 +58,7 @@ describe('deepClone',() => {
         return a + b
       }
       fn1.attr = { name: 'bibi',age: 18 }
-      const fn2 = deepClone(fn1)
+      const fn2 = new deepClone().clone(fn1)
       assert(fn1 !== fn2)
       assert(fn1.attr !== fn2.attr)
       assert(fn1.attr.name === fn2.attr.name)
@@ -68,7 +68,7 @@ describe('deepClone',() => {
     it('能够复制环',() => {
       const a1 = { name: 'bibi' }
       a1.self = a1
-      const a2 = deepClone(a1)
+      const a2 = new deepClone().clone(a1)
       assert(a1 !== a2)
       assert(a1.name === a2.name)
       assert(a1.self !== a2.self)
@@ -81,14 +81,14 @@ describe('deepClone',() => {
         b = b.child
       }
       console.log(a)
-      const a2 = deepClone(a)
+      const a2 = new deepClone().clone(a)
       assert(a !== a2)
       assert(a.child !== a2.child)
     })
     it('能复制正则表达式',() => {
       const a = /hi\d+/gi
       a.name = 'bibi'
-      const a2 = deepClone(a)
+      const a2 = new deepClone().clone(a)
       assert(a !== a2)
       assert(a.name === a2.name)
       assert(a.source === a2.source)
@@ -97,7 +97,7 @@ describe('deepClone',() => {
     it('能复制正则日期',() => {
       const a = new Date()
       a.name = 'bibi'
-      const a2 = deepClone(a)
+      const a2 = new deepClone().clone(a)
       assert(a !== a2)
       assert(a.getTime() === a2.getTime())
       assert(a.name === a2.name)
@@ -105,7 +105,7 @@ describe('deepClone',() => {
     it('跳过原型属性 ',() => {
       const a = Object.create({ name: 'bibi' })
       a.age = 18
-      const a2 = deepClone(a)
+      const a2 = new deepClone().clone(a)
       assert(a !== a2)
       assert.isFalse('name' in a2)
       assert(a.age === a2.age)
