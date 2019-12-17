d4bd60fe4789e9534f4f643ba3ee4eeb705d6ec7
成都-方欣芮 <sssrrr@vip.qq.com>
2019-12-17 14:09:34 +0800

    :white_check_mark:  添加 promise A+ 测试用例 2.2.1-2.2.6

--- src/promise.ts
+++ src/promise.ts
@@ -1,31 +1,52 @@
+const STATUS = {
+  PENDING: 'pending',
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected'
+}
+
 export default class Promise {
-  succeed = null
-  fail = null
+  callBacks = [] // 存放 succeed,fail
+  state = STATUS.PENDING
 
-  reject() {
+  resolve(result) {
+    if (this.state !== STATUS.PENDING) return
+    this.state = STATUS.FULFILLED
+
     setTimeout(() => {
-      this.fail()
+      this.callBacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined,result)
+        }
+      })
     })
   }
 
-  resolve() {
+  reject(reason) {
+    if (this.state !== STATUS.PENDING) return
+    this.state = STATUS.REJECTED
+
     setTimeout(() => {
-      this.succeed()
+      this.callBacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined,reason)
+        }
+      })
+
     })
   }
 
-  constructor(fn: object) {
+  constructor(fn) {
     if (typeof fn !== 'function') {
       throw new Error('必须接受函数作为参数')
     }
+    // fn 立即执行
     fn(this.resolve.bind(this),this.reject.bind(this))
   }
 
-  then(succeed?: object,fail?: object) {
-    if (!(typeof succeed == 'function' || typeof fail == 'function')) {
-      throw new Error('必须接受函数作为参数')
-    }
-    this.succeed = succeed
-    this.fail = fail
+  then(succeed?,fail?) {
+    const handle = []
+    if (typeof succeed === 'function') handle[0] = succeed
+    if (typeof fail === 'function') handle[1] = fail
+    this.callBacks.push(handle)
   }
 }
--- test/promise.ts
+++ test/promise.ts
@@ -1,7 +1,11 @@
 import * as chai from 'chai'
-import Promise from '../src/promise'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
 
 const assert = chai.assert
+import Promise from '../src/promise'
 
 describe('Promise',() => {
   it('是一个类',() => {
@@ -19,30 +23,104 @@ describe('Promise',() => {
     assert.isFunction(promise.then)
   })
   it('new Promise(fn) 中的 fn立即执行',() => {
-    let isCall = false
-    const promise = new Promise(() => {
-      isCall = true
-    })
-    assert.isTrue(isCall)
+    let isCall = sinon.fake()
+    const promise = new Promise(isCall)
+    assert.isTrue(isCall.called)
   })
-  it('new Promise(fn) 函数接受两个参数,resolve/reject',() => {
-    const promise = new Promise((resolve,reject) => {
+  it('new Promise(fn) 函数接受两个参数,resolve/reject',(done) => {
+    new Promise((resolve,reject) => {
       assert.isFunction(resolve)
       assert.isFunction(reject)
+      done()
+
     })
   })
-  it('promise.then(success)中的 success 会在 resolve 被调用的时候执行',(down) => {
-    let isCall = false
-    const promise = new Promise((resolve,reject) => {
-      assert(isCall === false)
-      resolve()
-      setTimeout(() => {
-        assert(isCall === true)
-        down()
+  describe('2.2-then方法',() => {
+    it('2.2.1-succeed和fail都是可选的参数,如果不是函数,都忽略',() => {
+      const promise = new Promise((resolve) => {
+        resolve()
+      })
+      // @ts-ignore
+      promise.then(null,'123')
+    })
+    it('2.2.2-此函数必须在promise 完成(fulfilled)后被调用,并把promise 的result作为它的第一个参数',down => {
+      let isCall = sinon.fake()
+      const promise = new Promise((resolve,reject) => {
+        assert.isFalse(isCall.called)
+        resolve(2333)
+        resolve(9999)
+        setTimeout(() => {
+          assert(promise.state === 'fulfilled')
+          assert.isTrue(isCall.calledOnce)
+          assert(isCall.calledWith(2333))
+          down()
+        })
+      })
+      promise.then(isCall)
+    })
+    it('2.2.3-此函数必须在promise rejected后被调用,并把promise 的reason作为它的第一个参数',down => {
+      let isCall = sinon.fake()
+      const promise = new Promise((resolve,reject) => {
+        assert.isFalse(isCall.called)
+        reject(2333)
+        reject(9999)
+        setTimeout(() => {
+          assert(promise.state === 'rejected')
+          assert.isTrue(isCall.calledOnce)
+          assert(isCall.calledWith(2333))
+          down()
+        })
+      })
+      promise.then(null,isCall)
+    })
+    it('2.2.4-成功-在我的代码执行完之前，不得调用 onFulfilled和onRejected',(done) => {
+      let isCall = sinon.fake()
+      const promise = new Promise((resolve,reject) => {
+        resolve()
+        setTimeout(() => {
+          assert.isTrue(isCall.called)
+          done()
+        })
+      })
+      promise.then(isCall)
+      assert.isFalse(isCall.called)
+    })
+    it('2.2.4-失败-在我的代码执行完之前，不得调用 onFulfilled和onRejected',(done) => {
+      let isCall = sinon.fake()
+      const promise = new Promise((resolve,reject) => {
+        reject()
+        setTimeout(() => {
+          assert.isTrue(isCall.called)
+          done()
+        })
+      })
+      promise.then(null,isCall)
+      assert.isFalse(isCall.called)
+    })
+    it('2.2.5-with no this value(then中函数的 this 无值)(',() => {
+      const promise = new Promise((resolve) => {})
+      promise.then(function () {
+        'use strict'
+        assert(this === undefined)
       })
     })
-    promise.then(() => {
-      isCall = true
+    it('2.2.6-then可以在同一个promise里被多次调用',(done) => {
+      const fnLists = [sinon.fake(),sinon.fake(),sinon.fake()]
+      const promise = new Promise((resolve) => {
+        resolve()
+      })
+      promise.then(fnLists[0])
+      promise.then(fnLists[1])
+      promise.then(fnLists[2])
+      setTimeout(()=>{
+        assert(fnLists[0].called)
+        assert(fnLists[1].called)
+        assert(fnLists[2].called)
+        assert(fnLists[1].calledAfter(fnLists[0]))
+        assert(fnLists[2].calledAfter(fnLists[1]))
+        done()
+      })
     })
   })
+
 })
